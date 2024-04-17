6eb74ad965f377de8cdac0b0b6c425333131f40d
Ole Hendrix <ohendrix@f0r4s18.codam.nl>
2024-04-17 17:16:26 +0200

    veel brandjes

--- parsing2.c
+++ parsing2.c
@@ -71,7 +71,13 @@ void	trim_quotes(t_list **list)
 		return ;
 	while ((*list) != NULL)
 	{
-		(*list)->str = ft_strtrim((*list)->str, "\"");
+		if ((*list)->next != NULL && (*list)->next->str[0] == '\"')
+		{
+			(*list)->next->str = ft_strtrim((*list)->next->str, "\"");
+			(*list)->str = ft_strjoin((*list)->str, " ");
+			(*list)->str = ft_strjoin((*list)->str, (*list)->next->str);
+			(*list)->next = (*list)->next->next;
+		}
 		(*list) = (*list)->next;
 	}
 	*list = begin;
@@ -86,6 +92,12 @@ void	combine_node(t_list **list)
 		return ;
 	while (begin->next != NULL)
 	{
+		// if (!ft_strncmp(begin->str, "\" ", 2))
+		// {
+		// 	begin->str = ft_strjoin(begin->str, begin->next->str);
+		// 	begin->next = begin->next->next;
+		// 	combine_node(&begin);
+		// }
 		if (begin->str[0] != '\"')
 			begin = begin->next;
 		else if (begin->str[0] == '\"' && begin->str[ft_strlen(begin->str) -1] != '\"')
@@ -100,19 +112,26 @@ void	combine_node(t_list **list)
 	}
 	trim_quotes(list);
 }
+
 void init_files(t_command *command, char **tokens)
 {
 	int	i;
+	bool inquotes;
 
 	i = 0;
+	inquotes = false;
 	while (tokens[i] != NULL)
 	{
-		if (!ft_strncmp(tokens[i], "<", 2))
+		if (tokens[i][0] == '\"' && !inquotes)
+			inquotes = true;
+		if (ft_strlen(tokens[i]) > 1 && tokens[i][ft_strlen(tokens[i]) - 1] == '\"')
+			inquotes = false;
+		if (!ft_strncmp(tokens[i], "<", 2) && !inquotes)
 		{
 			command->infile = ft_strdup(tokens[i + 1]);
 			i++;
 		}
-		else if (!ft_strncmp(tokens[i], ">", 2))
+		if (!ft_strncmp(tokens[i], ">", 2) && !inquotes)
 		{
 			command->outfile = ft_strdup(tokens[i + 1]);
 			i++;
@@ -120,13 +139,20 @@ void init_files(t_command *command, char **tokens)
 		i++;
 	}
 }
+
+
 int ft_flags(char **tokens, int j)
 {
 	if (tokens[j + 1] == NULL && tokens[j][0] != '\"')
 		return (1);
-	if (!ft_strncmp(tokens[j + 1], "|", 2))
+	if (tokens[j + 1] == NULL && tokens[j][0] == '\"')
+	{
+		tokens[j] = ft_strtrim(tokens[j], "\"");
 		return (1);
-	if (!ft_strncmp(tokens[j + 1], ">", 2))
+	}
+	if (!ft_strncmp(tokens[j + 1], "|", 2) && tokens[j][0] != '\"')
+		return (1);
+	if (!ft_strncmp(tokens[j + 1], ">", 2) && tokens[j][0] != '\"')
 		return (1);
 	return (0);
 }
@@ -139,7 +165,7 @@ int	ft_checkflags(t_list **commands, char **tokens, int j)
 		return (0);
 	lastnode = *commands;
 	while (lastnode->next != NULL)
-		lastnode = lastnode->next;
+			lastnode = lastnode->next;
 	if (ft_flags(tokens, j))
 	{
 		if (j > 0 && ft_strncmp(tokens[j - 1], "|", 2))
@@ -175,28 +201,49 @@ void	addcommand(t_list **commands, char **tokens, int j)
 void	init_commands(t_command *command, char **tokens)
 {
 	int i;
+	bool inquotes;
 
 	i = 0;
+	inquotes = false;
 	while (tokens[i] != NULL)
 	{
-		if (!ft_strncmp(tokens[i], "<", 2) || !ft_strncmp(tokens[i], ">", 2))
-			i += 2;
-		else if (!ft_strncmp(tokens[i], "|", 2))
-		{
-			command->pipe = true;
+		if (tokens[i][0] == '\"')
+			inquotes = true;
+		if (ft_strlen(tokens[i]) > 1 && tokens[i][ft_strlen(tokens[i]) - 1] == '\"')
+			inquotes = false;
+		if ((!ft_strncmp(tokens[i], "<", 2) || !ft_strncmp(tokens[i], ">", 2)) && !inquotes)
 			i++;
-		}
-		else if (!ft_strncmp(tokens[i], "<<", 3))
+		else if (!ft_strncmp(tokens[i], "|", 2) && !inquotes)
+			command->pipe = true;
+		else if (!ft_strncmp(tokens[i], "<<", 3) && !inquotes)
 		{
 			command->here_doc = true;
 			command->delimiter = ft_strdup(tokens[i + 1]);
-			i += 2;
-		}
-		else
-		{
-			addcommand(&command->commands, tokens, i);
 			i++;
 		}
+		else
+			addcommand(&command->commands, tokens, i);
+		i++;
+	}
+}
+
+void combine_empty_quote(t_list **list)
+{
+	t_list *begin;
+
+	begin = *list;
+
+	if (begin == NULL)
+		return ;
+	while (begin->next != NULL)
+	{
+		if (!ft_strncmp(begin->str, "\"", 2))
+		{
+			begin->str = ft_strjoin(begin->str, begin->next->str);
+			begin->next = begin->next->next;
+		}
+		else
+			begin = begin->next;
 	}
 }
 
@@ -209,9 +256,11 @@ void fill_struct2(char *line, char **envp)
 	init_command(&command);
 	i = 0;
 	tokens = ft_split(line, ' ');
+
 	init_files(&command, tokens);
 	init_commands(&command, tokens);
+	combine_empty_quote(&command.commands);
 	combine_node(&command.commands);
-	pipex(&command, envp);
-// 	printstruct(&command);
+	// pipex(&command, envp);
+	printstruct(&command);
 }
